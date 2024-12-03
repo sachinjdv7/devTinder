6ffe8e5b62d5339b472d84b03b5acfc1df2b6af7
sachinjadhav <sachinjdv7@gmail.com>
2024-12-03 18:05:49 +0530

    playing with multiple route handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,21 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = 8082;
 
-app.get('/user', (req, res) => {
-  res.send({ name: 'sachin' });
-});
+// multiple route handlers
 
-app.post('/user', (req, res) => {
-  res.send('data successfully saved in db');
-});
-
-app.delete('/user', (req, res) => {
-  res.send('data successfully deleted in db');
-});
-
-app.use('/', (req, res) => {
-  res.send('Welecome to express server 🚀');
-});
+app.get('/user', [
+  (req, res, next) => {
+    console.log('first handler');
+    // res.send('1 response');
+    next();
+  },
+  (req, res, next) => {
+    console.log('second handler');
+    res.send('2 response');
+    // next();
+  },
+]);
 
 app.listen(PORT, () => {
   console.log(`🚀 Express server is listening on ${PORT}`);
