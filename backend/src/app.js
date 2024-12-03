bbb8ab16c8b46de22ab6695b881f04c81de2feca
sachinjadhav <sachinjdv7@gmail.com>
2024-12-03 22:09:05 +0530

    init database

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,23 @@ const { connectDb } = require('./config/database');
 const app = express();
 const PORT = 8082;
 
+const User = require('./models/user.model');
+app.post('/signup', async (req, res) => {
+  const user = new User({
+    firstName: 'sachin',
+    lastName: 'jadhav',
+    emailId: 'sachin@gmail.com',
+    password: 'sdafasdf3545',
+  });
+
+  try {
+    await user.save();
+    res.send('User added successfully in DB');
+  } catch (error) {
+    res.status(400).send('Failed to save user', error.message);
+  }
+});
+
 connectDb()
   .then(() => {
     app.on('error', (error) => {
