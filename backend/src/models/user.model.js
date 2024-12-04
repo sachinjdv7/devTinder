157900eae95b0578d355455de10788dedda20db4
sachinjadhav <sachinjdv7@gmail.com>
2024-12-04 19:38:33 +0530

    use validator at schema level

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new Schema(
   {
@@ -18,10 +19,20 @@ const userSchema = new Schema(
       required: true,
       lowercase: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Enter a valid emailId' + value);
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error('Enter a strong password' + value);
+        }
+      },
     },
     gender: {
       type: String,
@@ -35,6 +46,11 @@ const userSchema = new Schema(
       type: String,
       default:
         'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=',
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error('Enter a valid URL' + value);
+        }
+      },
     },
     skills: {
       type: [String],
