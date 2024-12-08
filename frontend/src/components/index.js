91642066548490d0e43838af8a731194056b2060
sachinjadhav <sachinjdv7@gmail.com>
2024-12-08 17:58:01 +0530

    - setup routing

---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Body } from './Body';
+export { default as NavBar } from './NavBar';
+export { default as Profile } from './Profile';
+export { default as Login } from './Login';
