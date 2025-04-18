3ffb12aa899b876b6307cb6c62f155ea079c6686
Sachin Jadhav <sachinjdv7@gmail.com>
2025-04-19 01:13:15 +0530

    dnd

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      'mongodb://127.0.0.1:27017/devTinder'
+      "mongodb+srv://sachinjdv7:sachin123@cluster0.xh9em6m.mongodb.net/devTinder"
     );
     console.log(
       `\n🚀 MongoDB connected DB HOST: ${connectionInstance.connection.host} `
     );
   } catch (error) {
-    console.log('MongoDB connection Failed:', error);
+    console.log("MongoDB connection Failed:", error);
     process.exit(1);
   }
 };
